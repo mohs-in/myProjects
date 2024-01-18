31debce94920834aa1c1de23345d545e163d4539
mohs-in <in.mohsin@outlook.com>
2024-01-18 21:55:49 +0530

    code for drawing a new card added

--- blackjack-game/index.js
+++ blackjack-game/index.js
@@ -2,15 +2,22 @@ var messageEl = document.getElementById("message-el");
 var sumEl = document.querySelector("#sum-el");
 var cardsEl = document.querySelector("#cards-el");
 let message = "";
-let firstCard = 10
-let secondCard = 7
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 
-
 function startGame() {
-    cardsEl.textContent = "Cards: " + firstCard + "  " + secondCard;
+    renderGame();
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for(var i=0;i<cards.length;i++) {
+        cardsEl.textContent += cards[i] + " ";
+    }
     sumEl.textContent = "Sum: " +  sum;
 
     if (sum <= 20) {
@@ -23,4 +30,21 @@ function startGame() {
         // isAlive = false;
     }
     messageEl.textContent = message;
+}
+
+function newCard() {
+    console.log("Drawing a new card from the deck!");
+    var cardVar = getRandomCard();
+    sum += cardVar;
+    cards.push(cardVar);
+    renderGame();
+}
+
+function getRandomCard() {
+    let  randomValue = Math.floor(Math.random() * 13) + 1;
+    if(randomValue == 1)
+        return 11;
+    else if(randomValue >=11 && randomValue<=13) 
+        return 10;
+    else return randomValue; 
 }
