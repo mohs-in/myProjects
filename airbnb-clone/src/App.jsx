502f33498465f76789b784e0e22a54930e306f1c
mohs-in <in.mohsin@outlook.com>
2024-02-17 23:08:37 +0530

    updating jsx and css

--- airbnb-clone/src/App.jsx
+++ airbnb-clone/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import NavBar from '../components/NavBar.jsx'
+import Hero from '../components/Hero.jsx'
 
 function App() {
   return (
     <div>
-      
+      <NavBar />
+      <Hero />
     </div>
   )
 }
