a736bb5e23ccccb4d31e1038b5c8f14191243da9
Mohsin <in.mohsin@outlook.com>
2024-02-19 12:03:52 +0530

    Update App.jsx

--- airbnb-clone/src/App.jsx
+++ airbnb-clone/src/App.jsx
@@ -4,17 +4,22 @@ import Hero from '../components/Hero.jsx'
 import Card from '../components/Card.jsx'
 
 function App() {
+  const cards = data.map(item => {
+    return (
+      <Card 
+        img={item.coverImg} 
+        rating={item.stats.rating} 
+        reviewCount={item.stats.reviewCount} 
+        country={item.location}
+        title={item.title}
+        price={item.price} />
+    )
+  })
   return (
     <div>
       <NavBar />
       <Hero />
-      <Card 
-        img="katie-zaferes.png" 
-        rating="5.0" 
-        reviewCount={6} 
-        country="USA"
-        title="Life lessons with Katie Zaferes"
-        price={136} />
+      {cards}
     </div>
   )
 }
