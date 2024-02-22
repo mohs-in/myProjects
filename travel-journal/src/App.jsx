7f5168ed95c1548440156aee06067952d317a94d
mohs-in <in.mohsin@outlook.com>
2024-02-22 22:40:38 +0530

    added header

--- travel-journal/src/App.jsx
+++ travel-journal/src/App.jsx
@@ -3,9 +3,12 @@ import data from './data.js'
 
 function App() {
   return (
-    <div>
-      
+    <div className="main--component">
+      <div className='final-score'>
+        <div className="header"> <h4 className='header--title'>✈️  my travel journal.</h4></div>
+      </div>
     </div>
+    
   )
 }
 
