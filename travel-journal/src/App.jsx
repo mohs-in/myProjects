1c96fb04bb14a686c328ccd27dcc387489b4fdbe
mohs-in <in.mohsin@outlook.com>
2024-02-24 14:38:39 +0530

    update App.jsx

--- travel-journal/src/App.jsx
+++ travel-journal/src/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import data from './data.js'
+import Hero from '../components/Hero.jsx'
+import NavBar from '../components/NavBar.jsx'
 
 function App() {
+  const tiles = data.map(tile => {
+    return (
+      <Hero 
+        key= {tile.id}
+        {...tile}
+      />
+    )
+  })
+
   return (
-    <div className="main--component">
-      <div className='final-score'>
-        <div className="header"> <h4 className='header--title'>✈️  my travel journal.</h4></div>
+    <div className="final-score">
+      <div className="main--component">
+        <NavBar />
+      
+        <section className="tiles">
+          {tiles}  
+        </section>
+
       </div>
     </div>
     
